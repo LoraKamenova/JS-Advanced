d395cf562f68acf03573d5fd60e17c3069cf7460
Lora <cruzeiro@abv.bg>
2021-02-10 16:02:28 +0200

    Initial commit

---
+++ 1. Syntax_Lab/04. Largest Number.js
@@ -0,0 +1,8 @@
+function solve(num1, num2, num3) {
+    const maxNumber = Math.max(num1, num2, num3);
+
+    console.log(`The largest number is ${maxNumber}.`);
+}
+
+solve(5, -3, 16);
+solve(-3, -5, -22.5);
