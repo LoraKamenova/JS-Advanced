68fdf5aac2ee91353fc14bf0a5a93183aaac14f3
Lora <cruzeiro@abv.bg>
2021-02-11 20:53:19 +0200

    Initial commit

---
+++ 1. Syntax_Exercise/01. Fruit.js
@@ -0,0 +1,9 @@
+function solve(fruit, weight, price) {
+    const weightKg = (weight / 1000);
+    const money = (weightKg * price);
+
+    console.log(`I need $${money.toFixed(2)} to buy ${weightKg.toFixed(2)} kilograms ${fruit}.`);
+}
+
+solve('orange', 2500, 1.80);
+solve('apple', 1563, 2.35);
