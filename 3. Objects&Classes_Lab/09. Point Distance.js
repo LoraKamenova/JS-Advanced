e38ef94eef55ec9acc6ffbb5e572ceddd0986beb
Lora <cruzeiro@abv.bg>
2021-02-14 20:02:56 +0200

    Initial commit

---
+++ 3. Objects&Classes_Lab/09. Point Distance.js
@@ -0,0 +1,13 @@
+class point {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    static distance(a, b) {
+        const dx = a.x - b.x;
+        const dy = a.y - b.y;
+        return Math.hypot(dx, dy);
+    }
+
+}
