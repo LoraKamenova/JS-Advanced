c97bb5cef190edb4d149129864ba160a194087f5
Lora <cruzeiro@abv.bg>
2021-02-19 20:07:25 +0200

    Initial commit

---
+++ 6. Function context/01. Company.js
@@ -0,0 +1,69 @@
+class Company {
+    constructor() {
+        this.departments = []
+    }
+
+    addEmployee(username, salary, position, department) {
+        this._validateParam(username);
+        this._validateParam(salary);
+        this._validateParam(position);
+        this._validateParam(department);
+
+        if (salary < 0) {
+            throw new Error('Invalid input!');
+        }
+        let current = this.departments.find(d => d.name === department);
+
+        if (current === undefined) {
+            current = {
+                name: department,
+                employees: []
+            };
+            this.departments.push(current);
+        }
+        current.employees.push({
+            username,
+            salary,
+            position
+        });
+        return `New employee is hired. Name: ${username}. Position: ${position}`;
+    }
+
+    bestDepartment() {
+        const departments = this.departments.map(d => {
+            const dep = {
+                name: d.name,
+                employees: d.employees.slice(),
+            };
+            let total = 0;
+            for (let employee of dep.employees) {
+                total += employee.salary;
+            }
+            dep.averageSalary = total / dep.employees.length;
+
+            return dep;
+
+            // const dep = Object.assign({}, d);
+            // dep.averageSalary = d.employees.reduce((p, c, i, a) => p + c.salary, 0) / d.employees.length;
+            // return dep;
+        });
+        departments.sort((a, b) => b.averageSalary - a.averageSalary);
+
+        const best = departments[0];
+        if (best !== undefined) {
+            best.employees.sort((a, b) => b.salary - a.salary || a.username.localeCompare(b.username));
+            const result = [
+                `Best Department is: ${best.name}`,
+                `Average salary: ${best.averageSalary.toFixed(2)}`,
+            ];
+            best.employees.forEach(e => result.push(`${e.username} ${e.salary} ${e.position}`));
+            return result.join('\n');
+        }
+    }
+
+    _validateParam(param) {
+        if (param === '' || param === undefined || param === null) {
+            throw new Error('Invalid input!');
+        }
+    }
+}
