e1f637d3fd5c996eddfdeaa3808264e276b2957a
Lora <cruzeiro@abv.bg>
2021-02-22 20:45:47 +0200

    Initial commit

---
+++ 8. Object Composition/01. Heroes.js
@@ -0,0 +1,39 @@
+function solve() {
+    //деклариране на функции
+    function fighter(name) {
+        const instance = {
+            name,
+            health: 100,
+            stamina: 100,
+            fight
+        };
+
+        function fight() {
+            instance.stamina--;
+            console.log(`${instance.name} slashes at the foe!`);
+        }
+
+        return instance;
+    }
+
+    function mage(name) {
+        const instance = {
+            name,
+            health: 100,
+            mana: 100,
+            cast
+        };
+
+        function cast(spell) {
+            instance.mana--;
+            console.log(`${instance.name} case ${spell}`);
+        }
+
+        return instance;
+    }
+
+    return {
+        fighter,
+        mage
+    };
+}
