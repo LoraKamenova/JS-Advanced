bb791005cc934c6362f8d041d686bec36b2dbc9f
Lora <cruzeiro@abv.bg>
2021-02-23 21:13:16 +0200

    Initial commit

---
+++ 5. Dom_Manipulation_Exercise/01. Sections/script.js
@@ -0,0 +1,19 @@
+function create(words) {
+
+   const content = document.getElementById('content');
+   for (const word of words) {
+      let div = document.createElement('div');
+      let paragraph = document.createElement('p');
+      paragraph.textContent = word;
+
+      paragraph.style.display = "none";
+      div.appendChild(paragraph);
+
+      div.addEventListener('click', changeDisplay);
+
+      function changeDisplay() {
+         paragraph.style.display = "block";
+      }
+      content.appendChild(div);
+   }
+}
